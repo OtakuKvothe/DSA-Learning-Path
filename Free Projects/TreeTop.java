a4914abb80fd8fbb27c7f44e3b4dd9da442db1db
Swarnendu Basak <basakswarnendu@gmail.com>
2021-10-22 00:40:06 +0530

    Update TreeTop.java
    Comments for explaining the code

--- Free Projects/TreeTop.java
+++ Free Projects/TreeTop.java
@@ -14,15 +14,12 @@ class Node {
 }
 
 class TreeTop {
-
-    /*
-     * 
-     * class Node int data; Node left; Node right;
-     */
     public static void topView(Node root) {
         /*
          * Here we create a custom class to store each tree node along with a designated
-         * level
+         * level which are used to denote how much farther each node is to teh left or
+         * right of the root node. The root node is at 0, its left child at -1, right
+         * child at +1 and so on.
          */
         class qObj {
             Node node;
@@ -33,8 +30,14 @@ class TreeTop {
                 this.lvl = lvl;
             }
         }
-
+        /*
+         * The objective of the queue is to establish an order for visiting nodes. The
+         * order is preorder. Each node is assigned a lvl value relative to it's parent.
+         */
         Queue<qObj> q = new LinkedList<>();
+
+        // TreeMap to present the node values in sorted order of their lvl values, so
+        // that we have neatly ordered elements form leftmost to rightmost.
         TreeMap<Integer, Node> map = new TreeMap<>();
 
         if (root == null) {
@@ -45,6 +48,20 @@ class TreeTop {
 
         while (!q.isEmpty()) {
             qObj qt = q.poll();
+
+            /*
+             * Whenever we have a node whose lvl value has not been reached, it is added to
+             * map. This always works because of two considerations: there is only one
+             * unique map entry for every possible lv value and that the upper nodes are
+             * visited before the lower nodes.
+             * 
+             * Any 2 nodes with same lvl value, the node lower in the hirearchy will not
+             * enter the map because the node which is higher in the hierarchy has already
+             * entered the map.
+             * 
+             * Conversely, anytime a node at lower levels achieves a lvl value not reached
+             * by the upper level nodes, it may enter the map.
+             */
             if (!map.containsKey(qt.lvl)) {
                 map.put(qt.lvl, qt.node);
             }
@@ -58,6 +75,7 @@ class TreeTop {
             }
         }
 
+        // printing out the sorted values
         for (Map.Entry<Integer, Node> entry : map.entrySet()) {
             System.out.print(entry.getValue().data + " ");
         }
