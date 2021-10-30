f4320c7208754c1d441e53be4205944373665c5f
Swarnendu Basak <36073547+OtakuKvothe@users.noreply.github.com>
2021-10-30 23:12:19 +0530

    largest laddu problem

---
+++ Codechef/LARGESTLADDU.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LARGESTLADDU {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0){
+                long n = Math.pow(2, sc.nextInt());
+                HashMap<Integer, Integer> map = new HashMap<>();
+                for(int i = 0; i < n; i++){
+                    
+                }
+                Arrays.sort();
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+}
