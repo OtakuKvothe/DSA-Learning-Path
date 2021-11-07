f869cf05ada895a0999b7f4459fa09dda183ccaf
Swarnendu Basak <36073547+OtakuKvothe@users.noreply.github.com>
2021-11-07 15:21:22 +0530

    Create MAXBRIDGE.java

---
+++ Codechef/MAXBRIDGE.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+
+public class MAXBRIDGE {
+    public static void main(String[] args)throws java.lang.Exception {
+        try {
+            Scanner sc=new Scanner(System.in);
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+}
