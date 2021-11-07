3e1ec5f2ad38e87b9b2a8238ddcac5e22138501f
Swarnendu Basak <36073547+OtakuKvothe@users.noreply.github.com>
2021-11-07 12:31:13 +0530

    Create WILDRPL.java

---
+++ Codechef/WILDRPL.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+public class WILDRPL {
+    public static void main(String[] args) throws java.lang.Exception{
+        try {
+            Scanner sc=new Scanner(System.in);
+            int t=sc.nextInt();
+            while(t-->0){
+                
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+}
