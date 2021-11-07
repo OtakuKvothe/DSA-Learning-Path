d52c4e4e41cf46e662379633594b889b65963c36
Swarnendu Basak <36073547+OtakuKvothe@users.noreply.github.com>
2021-11-07 09:39:44 +0530

    Hill sequence updated working solution

--- Codechef/HILLSEQ.java
+++ Codechef/HILLSEQ.java
@@ -1,11 +1,13 @@
 /* package codechef; // don't place package name! */
 
+//It always gets stuck on something stupid.
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+class HILLSEQ
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -17,7 +19,7 @@ class Codechef
 		        int n=sc.nextInt();
 		        TreeMap<Integer, Integer> map = new TreeMap<>();
 		        int max = Integer.MIN_VALUE;
-		        for(int i=0; i<0; i++){
+		        for(int i=0; i<n; i++){
 		            int k=sc.nextInt();
 		            if(!map.containsKey(k)){
 		                map.put(k, 1);
@@ -29,22 +31,54 @@ class Codechef
 		        }
 		        int[] res = new int[n];
 		        int l=0, r=n-1;
-		        for(TreeMap<Integer, Integer> key: map.keySet()){
+                System.out.println("Max: "+max);
+                
+		        for(Integer key: map.keySet()){
                     int freq = map.get(key);
-                    if(freq==1){
-                        res[r]=freq;
-                        res--;
-                    }
-                    else{
-                        if(freq==2){
-                            res[l]=key;
-                            res[r]=key;
-                            l++;
-                            r--;
+                    System.out.println("l: "+l+" r: "+r);
+                    System.out.println("Key: "+key);
+                    if(l<=r){
+                        if(key!=max){
+                            if(freq==1){
+                                res[r--]=key;
+                            }
+                            else if(freq==2){
+                                res[r--]=key;
+                                res[l++]=key;
+                            }
+                            else{
+                                System.out.println("key != max && >=3");
+                                l=-1;
+                                break;
+                            }
+                        }
+                        else{
+                            if(freq==1){
+                                res[r--]=key;
+                            }
+                            else{
+                                System.out.println("key = max && freq>1");
+                                l=-1;
+                                break;
+                            }
                         }
                     }
+                    else{
+                        System.out.println("l>r error");
+                        l=-1;
+                        break;
+                    }
                 }
+                if(l==-1){
+                    System.out.println(-1);
+                    continue;
+                }
+                for(int i=0; i<res.length; i++){
+                    System.out.print(res[i]+" ");
+                }
+                System.out.println();
 		    }
+            sc.close();
 		} catch(Exception e) {
 		}
 	}
