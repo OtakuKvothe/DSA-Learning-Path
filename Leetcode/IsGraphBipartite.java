bd883700a8ae2f7a2d561cbcae6da841d85b2fab
Swarnendu Basak <36073547+OtakuKvothe@users.noreply.github.com>
2022-04-30 02:48:11 +0530

    Grind

---
+++ Leetcode/IsGraphBipartite.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class IsGraphBipartite {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] col = new int[n];
+        //-1 = blue
+        //1 = red
+        //0 = unmarked
+        boolean[] vis = new boolean[n];       
+        for(int i=0; i<n; i++) {
+            if(vis[i]) continue;
+            Queue<Integer> q = new LinkedList<>();
+            q.add(i);
+            int curr = 1;
+            vis[i] = true;
+            col[i] = curr;
+            while(!q.isEmpty()) {
+                int l = q.size();
+                curr = curr == 1 ? -1 : 1;
+                while(l-->0) {
+                    int v = q.poll();                    
+                    for(int a: graph[v]) {
+                        if(!vis[a]) {
+                            q.add(a);
+                            col[a] = curr;
+                            vis[a] = true;
+                        } else if(col[a] == col[v]) {
+                            return false;
+                        }
+                    }
+                }
+                
+            }
+        }
+        return true;
+    }
+}
--- Leetcode/UndergroundSystem.java
+++ Leetcode/UndergroundSystem.java
@@ -1,6 +1,25 @@
 //https://leetcode.com/problems/design-underground-system/
 
 import java.util.HashMap;
+import java.util.Map;
+
+class Pair<T1, T2> {
+    private T1 key;
+    private T2 value;
+
+    public Pair(T1 key, T2 value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public T1 getKey() {
+        return key;
+    }
+
+    public T2 getValue() {
+        return value;
+    }
+}
 
 public class UndergroundSystem {
     Map<Integer, Pair<String, Integer>> checkInMap = new HashMap<>();  // Uid - {StationName, Time}
