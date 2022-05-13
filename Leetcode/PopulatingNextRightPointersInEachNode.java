0670f84ce7c083d371712abc12fe1280ae8c5662
Swarnendu Basak <36073547+OtakuKvothe@users.noreply.github.com>
2022-05-13 20:24:54 +0530

    Stuff

---
+++ Leetcode/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PopulatingNextRightPointersInEachNode {
+    public Node connect(Node root) {
+        if(root == null) return root;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            int l = q.size();
+            while(l-- > 0) {
+                Node t = q.poll();
+                if(l>0) {
+                    t.next = q.peek();
+                } 
+                if(t.left != null) q.add(t.left);
+                if(t.right != null) q.add(t.right);
+            }
+        }       
+        return root;
+    }
+}
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
